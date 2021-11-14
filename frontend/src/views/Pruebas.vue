30ce49525d3d9fc0bc29ca949c4104fd7be8e765
lauriy91 <lauriys91@gmail.com>
2021-11-14 16:23:30 -0500

    initialized backend

---
+++ frontend/src/views/Pruebas.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <notifications/>
+</div>
+</template>
+
+<script>
+import Notifications from '../components/Notifications.vue'
+
+  export default {
+    components: {
+        Notifications
+    },
+
+  }
+</script>
